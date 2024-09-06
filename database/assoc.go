4193ed498d3b3bc22f6644891e11bb7f6d60fb83
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-09-06 20:49:49 +0100

    A

--- database/assoc.go
+++ database/assoc.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -51,6 +52,9 @@ func (db Database) EditDc(dcOld, dcNew string) error {
 func (db Database) Put(assoc Association) error {
 	log.Println("Associating", assoc.DC, assoc.WA, assoc.JID)
 	_, err := db.sqlDB.Exec("INSERT INTO assoc (dc, wa, jid) VALUES (?, ?, ?)", assoc.DC, assoc.WA, assoc.JID)
+	if err != nil {
+		log.Println("Failed to associate", assoc, err)
+	}
 	return err
 }
 
@@ -61,6 +65,7 @@ func (db Database) Delete(assoc *Association) error {
 }
 
 func NewMySQL() Database {
+	fmt.Println("Connecting to MySQL")
 	cfg := mysql.Config{
 		User:   os.Getenv("MYSQL_USER"),
 		Passwd: os.Getenv("MYSQL_PASSWORD"),
